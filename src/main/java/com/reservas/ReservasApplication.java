8945041a9313e3bd3485a58a6b7f4bdf6cd0b090
Andres Casas <ingfabiocasas@gmail.com>
2023-04-14 16:39:30 -0500

    Update ReservasApplication.java

--- src/main/java/com/reservas/ReservasApplication.java
+++ src/main/java/com/reservas/ReservasApplication.java
@@ -1,26 +1,15 @@
 package com.reservas;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-import com.reservas.service.ClienteService;
-import com.reservas.service.MesaService;
-import com.reservas.service.ReservaService;
-
 @SpringBootApplication
 public class ReservasApplication implements  CommandLineRunner{
 	
-	@Autowired
-	private ReservaService reservasService;
-	@Autowired
-	private MesaService mesasService;
-	@Autowired
-	private ClienteService clienteService;
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReservasApplication.class, args);
 	}
