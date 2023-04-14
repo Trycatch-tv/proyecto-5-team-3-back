92c845f83843d3fdd90f34c2867b3c4625726b15
Andres Casas <ingfabiocasas@gmail.com>
2023-04-13 23:11:56 -0500

    Update ReservasApplication.java

--- src/main/java/com/reservas/ReservasApplication.java
+++ src/main/java/com/reservas/ReservasApplication.java
@@ -1,13 +1,29 @@
 package com.reservas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.reservas.model.service.ReservasService;
+
 @SpringBootApplication
-public class ReservasApplication {
+public class ReservasApplication implements  CommandLineRunner{
+	
+	@Autowired
+	private ReservasService reservasService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservasApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
